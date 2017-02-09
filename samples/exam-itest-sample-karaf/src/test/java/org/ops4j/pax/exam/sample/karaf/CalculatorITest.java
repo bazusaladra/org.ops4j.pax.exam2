bac82222c2520d44d5a05dddfe8b64336efbc615
Blazej Saladra <blazej.saladra@s3group.com>
2017-02-09 15:41:31 +0100

    ectsp karaf testing

--- samples/exam-itest-sample-karaf/src/test/java/org/ops4j/pax/exam/sample/karaf/CalculatorITest.java
+++ samples/exam-itest-sample-karaf/src/test/java/org/ops4j/pax/exam/sample/karaf/CalculatorITest.java
@@ -20,10 +20,7 @@ import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
 
-import java.io.File;
-
-import javax.inject.Inject;
-
+import org.apache.karaf.features.FeaturesService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,61 +29,121 @@ import org.ops4j.pax.exam.ConfigurationManager;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
-import org.ops4j.pax.exam.options.MavenUrlReference;
 import org.ops4j.pax.exam.sample8.ds.Calculator;
+import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
 
 @RunWith(PaxExam.class)
 public class CalculatorITest {
 
-    private static Logger LOG = LoggerFactory.getLogger(CalculatorITest.class);
+  private static Logger LOG = LoggerFactory.getLogger(CalculatorITest.class);
 
-    @Inject
-    protected Calculator calculator;
+  @Inject
+  protected Calculator calculator;
 
-    @Configuration
-    public Option[] config() {
-        MavenArtifactUrlReference karafUrl = maven()
-            .groupId("org.apache.karaf")
-            .artifactId("apache-karaf")
-            .version(karafVersion())
-            .type("zip");
+  @Inject
+  protected FeaturesService featuresService;
 
-        MavenUrlReference karafStandardRepo = maven()
+  @Inject
+  protected BundleContext bundleContext;
+
+  @Configuration
+  public Option[] config() {
+    MavenArtifactUrlReference karafUrl = maven()
+        .groupId("com.ectsp")
+        .artifactId("ectsp-karaf")
+        .version("1.0")
+        .type("zip");
+    MavenArtifactUrlReference karafOriginalUrl = maven()
+        .groupId("org.apache.karaf")
+        .artifactId("apache-karaf")
+        .version("4.0.8")
+        .type("zip");
+
+    return new Option[]{
+        // KarafDistributionOption.debugConfiguration("5005", true),
+        karafDistributionConfiguration()
+            .frameworkUrl(karafOriginalUrl)
+            .unpackDirectory(new File("target", "exam"))
+            .useDeployFolder(false),
+        keepRuntimeFolder(),
+        configureConsole().ignoreLocalConsole(),
+        features(maven()
             .groupId("org.apache.karaf.features")
             .artifactId("standard")
             .version(karafVersion())
             .classifier("features")
-            .type("xml");
-        return new Option[] {
-            // KarafDistributionOption.debugConfiguration("5005", true),
-            karafDistributionConfiguration()
-                .frameworkUrl(karafUrl)
-                .unpackDirectory(new File("target", "exam"))
-                .useDeployFolder(false),
-            keepRuntimeFolder(),
-            configureConsole().ignoreLocalConsole(),
-            features(karafStandardRepo , "scr"),
-            mavenBundle()
-                .groupId("org.ops4j.pax.exam.samples")
-                .artifactId("pax-exam-sample8-ds")
-                .versionAsInProject().start(),
-       };
-    }
+            .type("xml"), "scr"),
+        features(maven()
+                .groupId("com.ectsp")
+                .artifactId("ectsp-karaf-feature")
+                .version("1.0.0-SNAPSHOT")
+                .classifier("features")
+                .type("xml")
+//                ,"ectsp-pax-jdbc-pool-aries", "ectsp-pax-jdbc-pool-dbcp2", "quartz", "ectsp-activemq-client", "ectsp-karaf-feature"
+        ),
+        features(maven()
+            .groupId("com.ectsp")
+            .artifactId("ectsp-extensions-karaf-feature")
+            .version("1.0.0-SNAPSHOT")
+            .classifier("features")
+            .type("xml")),
+        features(maven()
+            .groupId("com.ectsp")
+            .artifactId("ectsp-spring-osgi-feature")
+            .version("1.0.0-SNAPSHOT")
+            .classifier("features")
+            .type("xml")),
+        mavenBundle()
+            .groupId("org.ops4j.pax.exam.samples")
+            .artifactId("pax-exam-sample8-ds")
+            .versionAsInProject().start(),
+    };
+  }
 
-    public static String karafVersion() {
-        ConfigurationManager cm = new ConfigurationManager();
-        String karafVersion = cm.getProperty("pax.exam.karaf.version", "3.0.0");
-        return karafVersion;
-    }
+  public static String karafVersion() {
+    ConfigurationManager cm = new ConfigurationManager();
+    String karafVersion = cm.getProperty("pax.exam.karaf.version", "4.0.8");
+    return karafVersion;
+  }
 
 
-    @Test
-    public void testAdd() {
-        int result = calculator.add(1, 2);
-        LOG.info("Result of add was {}", result);
-        Assert.assertEquals(3, result);
-    }
+  @Test
+  public void testAdd() throws Exception {
+
+    LOG.info("{}", featuresService.listFeatures());
+    LOG.info("{}", Arrays.asList(bundleContext.getBundles()));
+
+    featuresService.addRepository(URI.create("mvn:org.ops4j.pax.jdbc/pax-jdbc-features/0.9.0/xml/features"));
+    featuresService.addRepository(URI.create("mvn:org.drools/drools-karaf-features/6.3.0.Final/xml/features"));
+    featuresService.addRepository(URI.create("mvn:org.apache.activemq/activemq-karaf/5.13.4/xml/features"));
+    featuresService.addRepository(URI.create("mvn:com.ectsp/ectsp-karaf-feature/1.0.0-SNAPSHOT/xml/features"));
+    featuresService.addRepository(URI.create("mvn:com.ectsp/ectsp-extensions-karaf-feature/1.0.0-SNAPSHOT/xml/features"));
+    featuresService.addRepository(URI.create("mvn:com.ectsp/ectsp-spring-osgi-feature/1.0.0-SNAPSHOT/xml/features"));
+
+    System.out.print(Arrays.asList(featuresService.listRepositories()).stream().map(s->s.getURI()).collect(Collectors.toList()));
+
+//    featuresService.installFeature("ectsp-pax-jdbc-pool-aries");
+//    featuresService.installFeature("ectsp-pax-jdbc-pool-dbcp2");
+//    featuresService.installFeature("quartz");
+//    featuresService.installFeature("ectsp-activemq-client");
+    featuresService.installFeature("ectsp-karaf-feature");
+    featuresService.installFeature("ectsp-extensions-karaf-feature");
+    featuresService.installFeature("ectsp-spring-osgi-feature");
+
+    System.out.print(Arrays.asList(featuresService.listFeatures()));
+    System.out.print(Arrays.asList(bundleContext.getBundles()));
+
+    int result = calculator.add(1, 2);
+    LOG.info("Result of add was {}", result);
+    Assert.assertEquals(3, result);
+  }
 
 }
